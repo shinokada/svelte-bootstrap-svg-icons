41889809ec378c0c2a787d8737bbb9e9ea3af150
Shinichi Okada <okada.shin@gmail.com>
2023-07-13 21:05:20 +0200

    fix: update license

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,73 @@
+<script>
+  import '../app.postcss';
+  import { page } from '$app/stores';
+  import DarkMode from 'flowbite-svelte/DarkMode.svelte';
+  import Navbar from 'flowbite-svelte/Navbar.svelte';
+  import NavBrand from 'flowbite-svelte/NavBrand.svelte';
+  import NavLi from 'flowbite-svelte/NavLi.svelte';
+  import NavUl from 'flowbite-svelte/NavUl.svelte';
+  import NavHamburger from 'flowbite-svelte/NavHamburger.svelte';
+  import Footer from 'flowbite-svelte/Footer.svelte'
+  import FooterBrand from 'flowbite-svelte/FooterBrand.svelte'
+  import FooterLinkGroup from 'flowbite-svelte/FooterLinkGroup.svelte'
+  import FooterLink from 'flowbite-svelte/FooterLink.svelte'
+  import { MetaTags } from 'svelte-meta-tags';
+
+  $: activeUrl = $page.url.pathname;
+</script>
+
+<MetaTags
+  title="Svelte Bootstrap SVG Icons"
+  description="Bootstrap icon set for Svelte"
+  openGraph={{
+    type: 'website',
+    url: 'https://svelte-bootstrap-svg-icons.vercel.app/',
+    title: "Svelte Bootstrap SVG Icons",
+    description: "Bootstrap icon set for Svelte",
+    images: [{ url: 'https://open-graph-vercel.vercel.app/api/svelte-bootstrap-svg-icons', width: 1200, height: 630, alt: "Svelte Bootstrap SVG Icons" }],
+    site_name: 'Svelte Bootstrap SVG Icons'
+  }}
+  twitter={{
+    handle: '@shinokada',
+    cardType: 'summary_large_image',
+    title: "Svelte Bootstrap SVG Icons",
+    description: "Bootstrap icon set for Svelte",
+    image:'https://open-graph-vercel.vercel.app/api/svelte-bootstrap-svg-icons',
+    imageAlt: "Svelte Bootstrap SVG Icons"
+  }} />
+  
+<Navbar let:hidden let:toggle class="dark:bg-slate-700" >
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap text-2xl font-semibold text-primary-700 dark:text-primary-500">
+      Svelte Bootstrap SVG Icons
+    </span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/" active={activeUrl === '/'}>Home</NavLi>
+    <NavLi href="/icons" active={activeUrl === '/icons'}>Icons</NavLi>
+    <NavLi href="https://github.com/shinokada/svelte-bootstrap-svg-icons">GitHub</NavLi>
+    <NavLi href="https://svelte-svg-icons.vercel.app/">Icon sets</NavLi>
+  </NavUl>
+  <DarkMode />
+</Navbar>
+<div class="mx-16 mb-16">
+  <slot />
+</div>
+
+<Footer footerType="logo" class="dark:bg-slate-700 mx-8">
+  <div class="sm:flex sm:items-center sm:justify-between">
+    <FooterBrand
+    href="https://svelte-bootstrap-svg-icons.vercel.app/"
+    name="Svelte Bootstrap SVG Icons"
+    classSpan="text-primary-700 dark:text-primary-500"
+  />
+  <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <FooterLink href="/">Home</FooterLink>
+    <FooterLink href="/icons">Icons</FooterLink>
+    <FooterLink href="https://github.com/shinokada/svelte-bootstrap-svg-icons/blob/main/LICENSE">Licensing</FooterLink>
+    <FooterLink href="https://github.com/shinokada/svelte-bootstrap-svg-icons/">GitHub</FooterLink>
+  </FooterLinkGroup>
+  </div>
+</Footer>
+
