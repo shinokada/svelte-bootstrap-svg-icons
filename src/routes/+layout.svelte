2f8c33e94901c526b0227d0903d1d194c2ad7ceb
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-21 19:45:05 +0100

    fix: add activeUrl for Sidebar and NavUl after svelte-5-ui-lib update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,6 +35,11 @@
   import { Runatics } from 'runatics';
   import DynamicCodeBlockStyle from './utils/DynamicCodeBlockStyle.svelte';
 
+  let activeUrl = $state($page.url.pathname);
+  $effect(() => {
+    activeUrl = $page.url.pathname;
+  });
+
   type LiType = {
     name: string;
     href: string;
@@ -196,7 +201,7 @@
       </div>
     {/snippet}
     {#if lis}
-      <NavUl class={ulclass}>
+      <NavUl {activeUrl} class={ulclass}>
         {@render navLi(lis)}
       </NavUl>
     {/if}
@@ -206,6 +211,7 @@
 <div class="lg:flex">
   {#if urlsToIncludeSwitcherAndSidebar.some((path) => currentUrl.startsWith(path))}
     <Sidebar
+      {activeUrl}
       {isOpen}
       {closeSidebar}
       breakpoint="lg"
